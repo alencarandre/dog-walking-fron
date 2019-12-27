4dc12786cc99fca2be4397f401ec506c7d5934f9
André Alencar <andreluis88@gmail.com>
2019-12-27 01:16:20 -0300

    Set pages routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './pages/list/list.component';
+import { ScheduleComponent } from './pages/schedule/schedule.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'detail/:id', component: DetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
